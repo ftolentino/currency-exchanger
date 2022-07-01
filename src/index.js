6cda838822bdf856f56460d9649d8d8e43b8bc50
ftolentino <filmer.tolentino@gmail.com>
2022-07-01 14:11:33 -0700

    WIP testing for response

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+import CurrencyExchange from './js/CurrencyExchange';
 
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function () {
-  $('selector').submit(function (event)) {
-    event.preventDefault(); //if it's a form
-    // UI code here
+function getElements(response) {
+  if(response) {
+    console.log(response);
   }
+}
+
+async function makeApiCall(currency) {
+  const response = await CurrencyExchange.getCurrencyExchange(currency);
+  getElements(response);
+}
+
+$(document).ready(function () {
+  $('#currencyInput').submit(function (event) {
+    event.preventDefault();
+    let currencyEx = $("#currencyInput").val();
+    makeApiCall(currencyEx);
+  });
 });
