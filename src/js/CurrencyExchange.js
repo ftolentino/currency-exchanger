9e2fb9d65a60e0729e8d645667c896fd61192f5c
ftolentino <filmer.tolentino@gmail.com>
2022-07-01 11:31:00 -0700

    Switch to async await request

--- src/js/CurrencyExchange.js
+++ src/js/CurrencyExchange.js
@@ -1,17 +1,13 @@
 export default class CurrencyExchange {
-  static getCurrencyExchange() {
-    return new Promise(function(resolve, reject){
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+  static async getCurrencyExchange(currency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`); 
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
