2d7dee60fcf10905b9fd68dee72d009ae359bd56
ftolentino <filmer.tolentino@gmail.com>
2022-07-04 16:20:53 -0700

    Add form error validation warnings to the user

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
-import CurrencyExchange from './js/CurrencyExchange';
+import CurrencyExchange from "./js/CurrencyExchange";
 
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+
+function clearFields() {
+  $("#output").text("");
+  $(".showError").text("");
+}
 
 function getElements(response) {
-  if(response) {
-    const body = response;
-    let conversionRate = body.conversion_rate;
-    let usd = $("#usdInput").val();
-    console.log(conversionRate);
-    let result = conversion(usd, conversionRate);
-    console.log(result);
+  const body = response;
+  let conversionRate = body.conversion_rate;
+  let usd = $("#usdInput").val();
+  let result = conversion(usd, conversionRate);
+
+  if(!result) {
+    $(".showError").text(`Please complete the form. Enter a number and select a country.`);
+  }
+
+  if (response) {
+    $("#output").text(result);
   } else {
-    $(".showError").text(`there was an error processing your request; ${response}`);
+    $(".showError").text(
+      `there was an error processing your request; ${response}`
+    );
   }
 }
 
 $(document).ready(function () {
-  $('#form-button').click(function () {
+  $("#form-button").click(function () {
     let country = $("#country").val();
-    let amount = $("#usdInput").val();
-    
-    console.log(amount);
+    clearFields();
     makeApiCall(country);
   });
 });
@@ -33,7 +42,7 @@ async function makeApiCall(country) {
   getElements(response);
 }
 
-function conversion (amount, country) {
+function conversion(amount, country) {
   let countryCode = country;
   let usdAmount = amount;
 
