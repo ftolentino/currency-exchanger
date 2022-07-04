378aff049770d5e7005183c24b800b2e02de0495
ftolentino <filmer.tolentino@gmail.com>
2022-07-04 00:46:47 -0700

    Remove url variable, set pair conversion url

--- src/js/CurrencyExchange.js
+++ src/js/CurrencyExchange.js
@@ -1,8 +1,7 @@
 export default class CurrencyExchange {
   static async getCurrencyExchange(country) {
     try {
-      let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${country}`;
-      const response = await fetch(url);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${country}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
